be2fc3ddf0a62939f88917da306a8835e38cfcdc
Ronald Gouldner <gouldner@hawaii.edu>
2020-09-25 10:57:41 -1000

    added DB config

--- startFakeSMTP.sh
+++ startFakeSMTP.sh
@@ -1,5 +1,7 @@
 BASEDIR=`dirname $0`
 
+CLEAR=$1
+
 JAR_FILE=`find ${BASEDIR} -name fake-smtp-server-1.2.2.jar`
 
 mkdir -p ${BASEDIR}/log
@@ -14,6 +16,11 @@ if [ -f ${BASEDIR}/running.pid ]
 then
     echo "running.pid found so stopping first"
     ${BASEDIR}/stopFakeSMTP.sh
+fi
+
+if [ "${CLEAR}" == "clear" ]
+then
+    rm DB/testmails.db.*    
 fi
 
 java -Dspring.config.location=${BASEDIR}/app_config/application.properties -jar ${JAR_FILE} &> ${BASEDIR}/log/fake-smtp.log &
